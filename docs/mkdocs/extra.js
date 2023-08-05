ba957af341ca2616313d985add12940d84677d62
qingdog <1759765836@qq.com>
2023-08-05 13:22:29 +0800

    extra

--- docs/mkdocs/extra.js
+++ docs/mkdocs/extra.js
@@ -1,1 +1,14 @@
-<script src="https://unpkg.com/requirejs@2.3.6/require.js" ></script>
+function loadScriptsInOrder(scripts, index) {
+        index = index || 0;
+        if (index < scripts.length) {
+            var script = document.createElement('script');
+            script.src = scripts[index];
+            script.onload = function() {
+                loadScriptsInOrder(scripts, index + 1);
+            };
+            document.head.appendChild(script);
+        }
+    }
+    var scriptUrls = ["https://unpkg.com/requirejs@2.3.6/require.js"];
+    //scriptUrls.push("https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js");
+loadScriptsInOrder(scriptUrls);
